4c6b3d61315c3614fe82e3140f170eb575c69262
Aleš Pázner <pazny.develop@gmail.com>
2024-11-27 15:33:35 +0100

    calculation done with no CLR

--- src/Calc/Calc.jsx
+++ src/Calc/Calc.jsx
@@ -39,6 +39,12 @@ export default function Calc() {
         // }
     }
 
+    function showResult() {
+        setOut((out) => {
+            return eval(out)
+        })
+    }
+
     let getRow = function (rowData) {
         return rowData.map((text, i) => 
             <td key={i}>
@@ -51,7 +57,10 @@ export default function Calc() {
     <h2>Calculator WIP demo</h2>
     <table className="calculator">
         <thead>
-            <tr><th colSpan="4"><input id="cmdLine" type="text" name="cmdLine" value={out} readOnly={true} /></th></tr>
+            <tr>
+                <th colSpan="3"><input id="cmdLine" type="text" name="cmdLine" value={out} readOnly={true} /></th>
+                <th><button onClick={showResult}>=</button></th>
+                </tr>
         </thead>
         <tbody>
         {keyboard.map( function(row, i) {
