75fce9ed6b6c3aa332959e2620a5254d68cb2f9b
Aleš Pázner <pazny.develop@gmail.com>
2024-11-27 16:55:26 +0100

    cleaBtn, error hadling and cleanup

--- src/Calc/Calc.jsx
+++ src/Calc/Calc.jsx
@@ -3,38 +3,53 @@ import "./Calc.css"
 
 export default function Calc() {
 
-    const [out, setOut] = useState('');
+    const CLR_BTN = "clr"
 
+    const [out, setOut] = useState('')
+    const [err, setErr] = useState('')
 
     const keyboard = [
         ["7", "8", "9", "+"],
         ["4", "5", "6", "-"],
         ["1", "2", "3", "*"],
-        ["", "0", "", "/"],
+        ["", "0", CLR_BTN, "/"],
     ];
     
-    function calculate (inp) {
-        setOut(function (oldOut) {
-            return "" + oldOut + inp
-        })
+    function handleBtnClick (inp) {
+        if(inp === CLR_BTN){
+            setOut("")
+        }else {
+            setOut(function (oldOut) {
+                return "" + oldOut + inp
+            })
+        }
     }
 
     function showResult() {
         setOut((out) => {
-            return eval(out)
+            try{
+                let result = eval(out)
+                if(result !== undefined){
+                    setErr("")
+                    return result
+                }
+            } catch (e) {
+                setErr(e.message)
+            }
         })
     }
 
     let getRow = function (rowData) {
         return rowData.map((text, i) => 
             <td key={i}>
-                {text && <button className={(Number.isInteger(parseInt(text))) ? "number" : "symbol"} onClick={() => calculate(text)} text={text}>{text}</button>}
+                {text && <button className={(Number.isInteger(parseInt(text))) ? "number" : "symbol"} onClick={() => handleBtnClick(text)} text={text}>{text}</button>}
             </td>
         )
     }
 
     return <>
     <h2>Calculator WIP demo</h2>
+    {err && <><p style={{fontSize: "18px"}}><b>Error: </b><i style={{color: "red"}}>{err}</i></p><hr /></>}
     <table className="calculator">
         <thead>
             <tr>
@@ -44,7 +59,7 @@ export default function Calc() {
         </thead>
         <tbody>
         {keyboard.map( function(row, i) {
-            return <tr key={i}> {getRow(row)} </tr>
+            return <tr key={i}>{getRow(row)}</tr>
         })}
         </tbody>
     </table>
