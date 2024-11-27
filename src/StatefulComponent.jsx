dcd0ea4feed2539942b758ec91e05219db75a0cc
Aleš Pázner <pazny.develop@gmail.com>
2024-11-27 14:31:11 +0100

    examples of hook calling

--- src/StatefulComponent.jsx
+++ src/StatefulComponent.jsx
@@ -2,7 +2,11 @@ import { useState } from "react"
 
 export default function StatefulComponent() {
     const [count, setCount] = useState(0)
-    const increaseCount = () => setCount(count+1)
+    // const increaseCount = () => setCount(count+2)
+    // const increaseCount = () => setCount(count => count+2)
+    const increaseCount = () => setCount(function(old){
+        return old + 2
+    })
     return <>
         <h2>Stateful component</h2>
         <p>Count: <small>{count}</small></p>
