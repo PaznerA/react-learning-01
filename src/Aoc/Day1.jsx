81499a2d926907c51cc01c9359ca8780b5bf5b35
Aleš Pázner <pazny.develop@gmail.com>
2024-12-01 17:30:18 +0100

    reformat again xD

--- src/Aoc/Day1.jsx
+++ src/Aoc/Day1.jsx
@@ -4,6 +4,7 @@ export default function getDay1Out () {
     //load text as a char array
     const list = Array.from(NUMBER_LIST)
 
+    //init working props
     let sum = 0
     let rowCounter = 0
     let activeList = 1
@@ -11,20 +12,17 @@ export default function getDay1Out () {
 
     //separate two-columns of text into two arrays
     for(const char of list) {
-        if(char === "\n") {
+        if(char === " ") { activeList = 2 }
+        else if(char === "\n") {
             rowCounter++
             activeList = 1
-        } else if(char === " "){
-            activeList = 2
-        }else{
-            (separatedArr[activeList][rowCounter] !== undefined) 
-                ? separatedArr[activeList][rowCounter] += "" + char
-                : separatedArr[activeList][rowCounter] = ""
-        }
-    }
+        } else {
+            separatedArr[activeList][rowCounter] !== undefined 
+            ? separatedArr[activeList][rowCounter] += "" + char
+            : separatedArr[activeList][rowCounter] = ""
+    }}
 
     //iterate array and sum for each row the numInleftColumnValue * numInRightColumnOccurencesCount
     separatedArr[1].map((num) => (sum += num * separatedArr[2].filter((v) => (v === num)).length))
-
     return sum
 }
